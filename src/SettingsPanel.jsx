cd0cd07d98fd76d27da588791b6c53495a5e8e53
Arnas Jelizarovas <jelizarovas@gmail.com>
2024-01-02 23:44:02 -0800

    added take-in sheet

--- src/SettingsPanel.jsx
+++ src/SettingsPanel.jsx
@@ -1,6 +1,13 @@
 import { Button, ButtonGroup, Checkbox, FormControlLabel } from "@mui/material";
 import React from "react";
-import { MdClear, MdFilterAlt, MdGridView, MdListAlt, MdSettings } from "react-icons/md";
+import {
+  MdClear,
+  MdFilterAlt,
+  MdGridView,
+  MdListAlt,
+  MdSettings,
+} from "react-icons/md";
+import { Link } from "react-router-dom";
 import { SettingsProvider, useSettings } from "./SettingsContext";
 
 export const SettingsPanel = ({ setSettingsOpen }) => {
@@ -16,38 +23,90 @@ export const SettingsPanel = ({ setSettingsOpen }) => {
   };
 
   return (
-    <div className="w-full mx-auto md:w-96 md:mx-2 mb-4 rounded border-white border-opacity-20 border  py-0.5">
+    <div className="container mx-auto md:w-96 md:mx-2 mb-4 rounded border-white border-opacity-20 border  py-0.5">
       <div className="flex justify-between items-center opacity-80 border-b border-white border-opacity-20 px-4 pb-0.5">
         <div className="flex items-center space-x-2">
           <MdSettings /> <span>Settings</span>
         </div>
-        <button type="button" className="flex" onClick={() => setSettingsOpen(false)}>
+        <button
+          type="button"
+          className="flex"
+          onClick={() => setSettingsOpen(false)}
+        >
           <MdClear />
         </button>
       </div>
       <div className="flex flex-col py-2 px-2">
-        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
           <Button
-            variant={settings.vehicleListDisplayMode === "grid" ? "contained" : "outlined"}
+            variant={
+              settings.vehicleListDisplayMode === "grid"
+                ? "contained"
+                : "outlined"
+            }
             onClick={() => handleViewChange("grid")}
           >
             <MdGridView /> <span className="px-2">Grid</span>
           </Button>
           <Button
-            variant={settings.vehicleListDisplayMode === "list" ? "contained" : "outlined"}
+            variant={
+              settings.vehicleListDisplayMode === "list"
+                ? "contained"
+                : "outlined"
+            }
             onClick={() => handleViewChange("list")}
           >
             <MdListAlt /> <span className="px-2">List</span>
           </Button>
         </ButtonGroup>
         <FormControlLabel
-          control={<Checkbox name="showPrice" checked={settings?.showPrice} onChange={handleChange} />}
+          control={
+            <Checkbox
+              name="showPrice"
+              checked={settings?.showPrice}
+              onChange={handleChange}
+            />
+          }
           label="Show Price"
         />
         <FormControlLabel
-          control={<Checkbox name="showCarfax" checked={settings?.showCarfax} onChange={handleChange} />}
+          control={
+            <Checkbox
+              name="showCarfax"
+              checked={settings?.showCarfax}
+              onChange={handleChange}
+            />
+          }
           label="Show Carfax"
         />
+      </div>
+      <div className="flex flex-col px-2 space-y-2">
+        <a
+          href="http://jelizarovas.github.io/HondaAccessory/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Honda Accessory
+        </a>
+        <a
+          href="http://jelizarovas.github.io/makey/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Make Key Tag
+        </a>
+        {/* <Link to="/pdi">PDI Templates</Link> */}
+        {/* <Link to="/pdi">Perfect Delivery</Link> */}
+        <a
+          href="pdf/Take-in Sheet.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Take-In Sheet
+        </a>
       </div>
     </div>
   );
