3cb01084b64fd1ca8853034c353ced5208e1a444
Arnas Jelizarovas <jelizarovas@gmail.com>
2025-03-17 20:38:06 -0700

    adding new menu bar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { LayoutAppBar } from "./LayoutAppBar";
 import { TitleProvider } from "./TitleContext";
+import { MenuBar } from "./components/MenuBar";
 
 const Layout = ({ children }) => {
   return (
     <TitleProvider>
-      <div className="layout">
+      <div className="relative layout">
         <LayoutAppBar />
+        {/* <MenuBar /> */}
         <main className="container mx-auto px-2 py-1">{children}</main>
       </div>
     </TitleProvider>
